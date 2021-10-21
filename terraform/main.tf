d6140f92a85ac844cbc9659666b997d8404257b2
Arseny Egorov <egoroff-ars@yandex.ru>
2021-10-21 11:46:56 +0300

    HW 07_03

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-west-2"
 
   tags = {
     Name = "my_subnet"
@@ -48,12 +48,41 @@ resource "aws_network_interface" "foo" {
   }
 }
 
+locals {
+  web_instance_type_map = {
+    stage = "t1.micro"
+    prod  = "t2.micro"
+  }
+
+  web_instance_count_map = {
+    stage = 1
+    prod  = 2
+  }
+
+  backend_instances = {
+    "t1.micro" = data.aws_ami.ubuntu.id
+    "t2.micro" = data.aws_ami.ubuntu.id
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = local.web_instance_type_map[terraform.workspace]
+  count         = local.web_instance_count_map[terraform.workspace]
 
   network_interface {
     network_interface_id = aws_network_interface.foo.id
     device_index         = 0
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_instance" "backend" {
+  for_each = local.backend_instances
+
+  ami           = each.value
+  instance_type = each.key
 }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "terraform-netology"
+    key    = "terraform"
+    region = "us-west-2"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
